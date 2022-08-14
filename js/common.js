d4e2d02fc2ae5edb2be52ff1467c5a28ef584a7d
Alex_web <30694838+AlexWeb-ux@users.noreply.github.com>
2022-08-14 17:48:12 +0300

    14/08 17/50

---
+++ js/common.js
@@ -0,0 +1,149 @@
+$(window).load(function() {
+	$(".loaderinner").delay(3000).fadeOut("slow"); 
+  $(".loader").delay(3200).slideUp("slow");
+});
+$(document).ready(function() {
+	//equalheight - одинаковая высота колонок
+	//var eqElement = ".cat_container > div, .home_news > div"
+	var eqElement = ".element"
+	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
+	//Попап менеджер FancyBox
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".top_mnu").navigation();
+	//Скролл до id, указанного в hash URL
+	var elem = window.location.hash;
+	if(elem) {
+		$.scrollTo(elem, 800, {
+			offset: -80
+		});
+	};
+	//Каруселька
+	function carousel_main() {
+		var owl = $(".owl-carousel");
+		owl.owlCarousel({
+			loop: true,
+			nav: true,
+			margin:0,
+			dots: true,
+			mouseDrag: false,
+			responsiveClass:true,
+      responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:true
+        }
+    }
+			
+		});
+		owl.on("mousewheel", ".owl-carousel", function (e) {
+			if (e.deltaY > 0) {
+				owl.trigger('prev.owl.carousel', [300]);
+			} else {
+				owl.trigger('next.owl.carousel');
+			}
+			e.preventDefault();
+		});
+		$(".next_button").click(function() {
+			owl.trigger('next.owl.carousel');
+		});
+		$(".prev_button").click(function() {
+			owl.trigger('prev.owl.carousel', [300]);
+		});
+		owl.on("resized.owl.carousel", function(event) {
+			var $this = $(this);
+			$this.find(".owl-height").css("height", $this.find(".owl-item.active").height());
+		});
+		setTimeout(function() {
+			owl.find(".owl-height").css("height", owl.find(".owl-item.active").height());
+		}, 5000);
+	};
+	carousel_main();
+	//Аякс отправка форм
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+	//Аудио
+	$(document).ready(function () {
+		$('.start-modal').fadeIn();
+		$('.dark-block').fadeIn();
+		$('body').addClass('overflow-hidden');
+	})
+
+	
+	$('#onSoundBtn').click(function () {
+		document.getElementById('audioFirst').play();
+		document.getElementById('1').classList.add('active');
+		$('.start-modal').fadeOut();
+		$('.dark-block').fadeOut();
+		$('body').removeClass('overflow-hidden');
+	})
+	
+	$('#offSoundBtn').click(function () {
+		$('.start-modal').fadeOut();
+		$('.dark-block').fadeOut();
+		$('body').removeClass('overflow-hidden');
+	})
+	$('.buttons a').click(function () {
+		if (this.classList.contains('active')) {
+			this.removeAttribute('class');
+			this.childNodes[1].pause();
+			this.childNodes[1].currentTime = 0;
+		} else {
+			this.classList.add('active');
+			$('.buttons a').not(this).removeClass('active');
+			for (let i = 0; i < $('.buttons a').not(this).length; i++) {
+				$('.buttons a').not(this).children('audio')[i].pause();
+				$('.buttons a').not(this).children('audio')[i].currentTime = 0;
+			}
+			this.childNodes[1].play();
+		}
+	})
+	/*likes*/
+	const btn = document.querySelector('.buy_likes');
+  let like = true,
+  likeCount = document.querySelector('.count').innerHTML;
+  btn.addEventListener('click', () => {
+  likeCount = like ? ++likeCount : --likeCount;
+  like = !like;
+  document.querySelector('.count').innerHTML = likeCount;
+  });
+	/*Tabs*/
+	$('.tab_header').on('click', accord);
+	function accord(){
+			$('.tab_text').not($(this).next()).slideUp(200);
+			$(this).next().slideToggle(300);
+		}	
+  /*Slider*/
+	$('.owl-stage .gallery_img').click(function(e) {
+		e.preventDefault();
+		$('.gallery_img').parent().parent().removeClass('flip');
+		$(this).parent().parent().addClass('flip');
+	});
+
+
+});
+
